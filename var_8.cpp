aace5e12f262bea446323ff35704f39932850c79
SlashTM <95855221+SlashTM@users.noreply.github.com>
2022-03-12 16:15:32 +0300

    Add files via upload

---
+++ var_8.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int GetNum(int num)
+{
+    _asm
+    {
+        mov eax, num
+        mov ebx, 10
+        mov edi, 2
+        mov ecx, 0
+        Loop1:
+        cdq
+            div ebx
+            push eax
+            mov eax, edx
+            cdq
+            div edi
+            test edx, edx; флаги pf - чётность zf - ноль sf - знак результата
+            jz Even; если остаток равен нулю то прыжок
+            Back :
+        pop eax
+            test eax, eax
+            jnz Loop1; если число не ноль то прыгаем в начало, иначе выходим
+            jmp Exit
+            Even :
+        inc ecx; прибавляем 1 к счётчику чётных цифр
+            jmp Back; продолжаем идти 
+            Exit :
+        mov eax, ecx
+    }
+}
+
+int main()
+{
+    cout << "Enter number: ";
+    int num;
+    cin >> num;
+    int evencount = GetNum(num);
+    cout << "This number has " << evencount << " even digits";
+}
